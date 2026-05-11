df29471fc8e409eecfb8a4aeb89011557b3331e5
Anshul Sharma <42910335+Anshul00Sharma@users.noreply.github.com>
2026-05-11 16:41:04 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        answer = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            cache = set()
+            not_in = set()
+            j = i+1
+            while j < len(nums):
+                val = (nums[i] + nums[j]) * -1
+                if val in cache:
+                    ans = [nums[i],val,nums[j]]
+                    if tuple(ans) not in not_in:
+                        answer.append(ans)
+                    not_in.add(tuple(ans))
+                cache.add(nums[j])
+                j+=1
+        return answer
