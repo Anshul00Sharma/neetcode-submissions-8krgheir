b7e8997c0955e1813cb0ceaec219a0e4f3cd49fb
Anshul Sharma <42910335+Anshul00Sharma@users.noreply.github.com>
2026-06-10 12:16:55 +0530

    Add: binary-tree-maximum-path-sum - submission-2

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        maxPath = float('-inf')
+        def helper(root) -> int:
+            nonlocal maxPath
+            if not root:
+                return 0
+            left = max(helper(root.left), 0)
+            right = max(helper(root.right), 0)
+            possibleMax = left + right + root.val
+            maxPath = max(maxPath,possibleMax)
+            return max(left + root.val,right + root.val)
+        helper(root)
+        return maxPath
